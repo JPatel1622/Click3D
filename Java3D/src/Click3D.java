0eccba207aacc20d5dc51bf1cbfba589a77a7791
JPatel1622 <pateljeel1622@yahoo.com>
2022-05-01 11:30:37 -0400

    Fixed stuff

--- Java3D/src/Click3D.java
+++ Java3D/src/Click3D.java
@@ -57,16 +57,7 @@ public class Click3D extends Applet implements MouseListener, ActionListener {
 
 	}
 
-	AmbientLight aLight;
-	DirectionalLight dLight;
-	PointLight pLight;
-	SpotLight sLight;
-	SpotLight sLight2;
 	PickCanvas pc;
-	PositionInterpolator translator;
-	TransformInterpolator transformInterpolator;
-	RotationInterpolator rotator;
-	BranchGroup root;
 	TransformGroup spin;
 	BoundingSphere bounds;
 	Alpha alpha;
@@ -218,9 +209,7 @@ public class Click3D extends Applet implements MouseListener, ActionListener {
 	}
 
 	private BranchGroup createSceneGraph() {
-		
-
-		root = new BranchGroup();
+		BranchGroup root = new BranchGroup();
 		root.setCapability(BranchGroup.ALLOW_DETACH);
 		root.setCapability(BranchGroup.ALLOW_CHILDREN_WRITE);
 
@@ -240,31 +229,31 @@ public class Click3D extends Applet implements MouseListener, ActionListener {
 		root.addChild(background);
 
 		// Ambient lighting
-		aLight = new AmbientLight(true, new Color3f(Color.CYAN));
+		AmbientLight aLight = new AmbientLight(true, new Color3f(Color.CYAN));
 		aLight.setInfluencingBounds(bounds);
 		aLight.setCapability(PointLight.ALLOW_STATE_WRITE | PointLight.ALLOW_STATE_READ);
 		root.addChild(aLight);
 
 		// Directional Lighting
-		dLight = new DirectionalLight(new Color3f(Color.RED), new Vector3f(0f, 1f, 0f));
+		DirectionalLight dLight = new DirectionalLight(new Color3f(Color.RED), new Vector3f(0f, 1f, 0f));
 		dLight.setCapability(PointLight.ALLOW_STATE_WRITE | PointLight.ALLOW_STATE_READ);
 		dLight.setInfluencingBounds(bounds);
 		root.addChild(dLight);
 
 		// Point Light
-		pLight = new PointLight(new Color3f(Color.YELLOW), new Point3f(-0.3f, 0.7f, 1f), new Point3f(1f, 0f, 0f));
+		PointLight pLight = new PointLight(new Color3f(Color.YELLOW), new Point3f(-0.3f, 0.7f, 1f), new Point3f(1f, 0f, 0f));
 		pLight.setCapability(PointLight.ALLOW_STATE_WRITE | PointLight.ALLOW_STATE_READ);
 		pLight.setInfluencingBounds(bounds);
 		root.addChild(pLight);
 
 		// Spot Light
-		sLight = new SpotLight(new Color3f(Color.BLACK), new Point3f(0.7f, 0.7f, 0.7f), new Point3f(1f, 0f, 0f),
+		SpotLight sLight = new SpotLight(new Color3f(Color.BLACK), new Point3f(0.7f, 0.7f, 0.7f), new Point3f(1f, 0f, 0f),
 				new Vector3f(-0.7f, -0.7f, -0.7f), (float) (Math.PI / 6.0), 0f);
 		sLight.setCapability(PointLight.ALLOW_STATE_WRITE | PointLight.ALLOW_STATE_WRITE);
 		sLight.setInfluencingBounds(bounds);
 		root.addChild(sLight);
 
-		// sLight2 = new SpotLight(new Color3f(Color.CYAN), new Point3f(0.7f, 0.7f,
+		// SpotLight sLight2 = new SpotLight(new Color3f(Color.CYAN), new Point3f(0.7f, 0.7f,
 		// -0.7f), new Point3f(1f, 0f, 0f),
 		// new Vector3f(-0.7f, -0.7f, 0.7f), (float) (Math.PI / 12.0), 128f);
 		// sLight2.setCapability(PointLight.ALLOW_STATE_WRITE |
@@ -273,7 +262,7 @@ public class Click3D extends Applet implements MouseListener, ActionListener {
 		// root.addChild(sLight2);
 
 		Alpha alp = new Alpha(-1, 10000);
-		rotator = new RotationInterpolator(alp, spin);
+		RotationInterpolator rotator = new RotationInterpolator(alp, spin);
 		rotator.setSchedulingBounds(bounds);
 
 		spin.addChild(rotator);
